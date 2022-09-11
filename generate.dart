9eac19ab2af0439a2dd8a0db09267ada74f06392
Saleh Alzahrani <saleh_ahmed@outlook.com>
2022-09-10 19:25:44 -0500

    fixes

--- generate.dart
+++ generate.dart
@@ -4,21 +4,7 @@ void main() {
   MyGenerator.generate(
     //packages app use
     usedPackages: [
-      Package.googleMaps,
-      Package.imagePicker,
-      Package.photoView,
-      Package.sharePlus,
-      Package.mapLauncher,
-      Package.appLinks,
-      Package.flutterSvg,
-      Package.firebaseCore,
-      Package.firebaseCrashlytics,
-      Package.firebaseMessaging,
-      Package.firebaseAuth,
-      Package.hive,
-      Package.appSettings,
-      Package.pinCodeFields,
-      Package.location,
+
     ],
   );
 }
