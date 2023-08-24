2c4699a5bb5437df89f0c99fa5358cae930e78da
Saleh Alzahrani <saleh_ahmed@outlook.com>
2023-08-24 17:48:11 -0500

    Update app_version_updater.dart

--- lib/src/app_version_updater.dart
+++ lib/src/app_version_updater.dart
@@ -3,14 +3,14 @@ import 'dart:io';
 import 'package:yaml_modify/yaml_modify.dart';
 
 class AppVersionUpdater {
-  static void update([String? path]) {
+  static void update({String? path, bool second = false}) {
     String fileContent = File(path ?? "pubspec.yaml").readAsStringSync();
     var yml = loadYaml(fileContent);
     var map = getModifiableNode(yml);
     var version = map["version"];
 
     print("Current version:" + version);
-    var newVersion = _updateVersion(version);
+    var newVersion = _updateVersion(version, second);
     print("Updated version:" + newVersion);
 
     map["version"] = newVersion;
@@ -18,7 +18,7 @@ class AppVersionUpdater {
     File(path ?? "pubspec.yaml").writeAsStringSync(toYamlString(map));
   }
 
-  static _updateVersion(version) {
+  static _updateVersion(String version, bool second) {
     var versionParts = version.split("+");
     var lastPart = versionParts.last;
     var lastPartInt = int.parse(lastPart);
@@ -26,11 +26,16 @@ class AppVersionUpdater {
 
     var firstPart = versionParts.first;
     var firstParts = firstPart.split(".");
+
     var fristLastPart = firstParts.last;
+    if (second) {
+      fristLastPart = firstParts[1];
+    }
+
     var fristLastPartInt = int.parse(fristLastPart);
     fristLastPartInt++;
 
-    firstParts[firstParts.length - 1] = fristLastPartInt.toString();
+    firstParts[firstParts.length - (second ? 2 : 1)] = fristLastPartInt.toString();
     var firstPartJoined = firstParts.join(".");
 
     var newVersion = firstPartJoined + "+" + lastPartInt.toString();
